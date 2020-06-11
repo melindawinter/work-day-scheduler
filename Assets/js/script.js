78d64cd3b6545dd3c3a6a51a835af8fcf568869a
Melinda Winter <melindawinter42@gmail.com>
2020-06-10 18:38:50 -0700

    styling the jumbotron

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,28 @@
+//Make sure the document has loaded
 $(document).ready(function () {
-  var dateEl = $("#currentDay");
-  var now = moment().format("dddd, MMM Do");
-  console.log(now);
-
+  //Create the date text in the jumbotron
+  var now = moment().format("dddd, MMMM Do");
   $("#currentDay").text(now);
+
+  //Create the hours
+  //   var hours = [
+  //     "9AM",
+  //     "10AM",
+  //     "11AM",
+  //     "12PM",
+  //     "1PM",
+  //     "2PM",
+  //     "3PM",
+  //     "4PM",
+  //     "5PM",
+  //   ];
+  //   var hoursDiv = $("#hour-div");
+
+  //   for (var i = 0; i < hours.length; i++) {
+  //     var newHourDiv = $("<div>" + hours[i] + "</div>");
+  //     hoursDiv.append(newHourDiv);
+  //     $(newHourDiv).addClass("hour-div");
+  //   }
+  //End (document).ready
 });
+// var hours = moment().hour(hours);
