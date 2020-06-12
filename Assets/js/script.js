43bace667da2719457afdcc67c7c19d02d3cabab
Melinda Winter <melindawinter42@gmail.com>
2020-06-11 17:52:45 -0700

    working on hours divs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,26 +3,25 @@ $(document).ready(function () {
   //Create the date text in the jumbotron
   var now = moment().format("dddd, MMMM Do");
   $("#currentDay").text(now);
+  //Create the array for the hours in the scheduler
+  var hours = [
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM",
+  ];
 
-  //Create the hours
-  //   var hours = [
-  //     "9AM",
-  //     "10AM",
-  //     "11AM",
-  //     "12PM",
-  //     "1PM",
-  //     "2PM",
-  //     "3PM",
-  //     "4PM",
-  //     "5PM",
-  //   ];
-  //   var hoursDiv = $("#hour-div");
-
-  //   for (var i = 0; i < hours.length; i++) {
-  //     var newHourDiv = $("<div>" + hours[i] + "</div>");
-  //     hoursDiv.append(newHourDiv);
-  //     $(newHourDiv).addClass("hour-div");
-  //   }
-  //End (document).ready
+  var currentHour = moment().format("h");
+  var hoursDiv = $("#hour-div");
+  for (var i = 0; i < hours.length; i++) {
+    var newHourDiv = $("<div>" + hours[i] + "</div>");
+    hoursDiv.append(newHourDiv);
+    $(newHourDiv).addClass("hour time-block row");
+    //$(newHourDiv).append((<"div" + );
+  
 });
-// var hours = moment().hour(hours);
