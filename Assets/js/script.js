a6b88bcc9902eb3a74e29e2c8c2e2856ef17ca00
Melinda Winter <melindawinter42@gmail.com>
2020-06-12 18:01:41 -0700

    momentjs functionality working on hours

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -16,11 +16,48 @@ $(document).ready(function () {
     "5PM",
   ];
 
-  var currentHour = moment().format("h");
+  // var currentHour = moment().format("h");
   var hoursDiv = $("#hour-div");
+  var hourId = 8;
   for (var i = 0; i < hours.length; i++) {
-    var newHourDiv = $("<div>" + hours[i] + "</div>");
-    hoursDiv.append(newHourDiv);
-    $(newHourDiv).addClass("hour time-block row");
+    hourId++;
+    var timeDiv = $("<div class='time'>" + hours[i] + "</div>");
+    var scheduleDiv = $(
+      "<div class='hour row time-block'> <textarea id=" +
+        hourId +
+        " class='description'></textarea> <button class='saveBtn'><i class='fas fa-save'></i></button> </div>"
+    );
+    hoursDiv.append(timeDiv);
+    hoursDiv.append(scheduleDiv);
   }
+  $(".saveBtn").on("click", function () {
+    var scheduleText = $(this).siblings(".description").val();
+    var scheduleTime = $(this).siblings(".description").attr("id");
+    localStorage.setItem(scheduleTime, scheduleText);
+    console.log(scheduleText);
+  });
+
+  function hourChecker() {
+    var currentHour = moment().hours();
+    $(".description").each(function () {
+      var hourEl = parseInt($(this).attr("id"));
+      console.log(hourEl);
+      if (hourEl < currentHour) {
+        $(this).addClass("past");
+      } else if (hourEl === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  hourChecker();
+
+  $("#9").val(localStorage.getItem("9"));
+  $("#10").val(localStorage.getItem("10"));
+  $("#17").val(localStorage.getItem("17"));
+  console.log(localStorage.getItem("9"));
 });
